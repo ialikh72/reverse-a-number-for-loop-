9031507da313e272546e2db38b3f1abe5f9de961
ialikh72 <ialikh.w1@gmail.com>
2026-02-03 22:17:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Reverse
+{
+    static void Main()
+    {
+        int rev=Reversee();
+        Console.WriteLine("R="+rev);
+    }
+    static int Reversee()
+    {
+        Console.WriteLine(" enter a number to reverse:");
+        int num=int.Parse(Console.ReadLine());
+        int rev = 0;
+        for (;num > 0;)
+        {
+            rev = rev * 10 + num % 10;
+            num=num / 10;
+        }
+        return rev;
+    }
+}
